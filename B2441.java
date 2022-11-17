e5b8e4cdcbc042477844782b752456bd89c7b089
추추 <117057960+choo091113@users.noreply.github.com>
2022-11-17 17:58:52 +0900

    Update B2441.java

--- B2441.java
+++ B2441.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
 
-public class B2441 {
+public class B2441 { // 별 찍기 - 4
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int count = 0;
+		int x = 0;
+		int y = 0;
+		
+		while (true) {
+		
+			for (int j = a-x; j < a; j++) {
+				System.out.print(" ");
+		}
+		
+			
+			for (int z = 0+y; z < a; z++) {
+				System.out.print("*");
+			}
+			
+			System.out.println();
+			x++;
+			y++;
+			count++;
+			if(a == count) break;
 	}
-
+}
 }
